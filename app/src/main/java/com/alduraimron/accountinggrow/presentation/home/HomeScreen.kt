f49eac0f29517b9b475e48d9f801570d945fbdcb
alduraimron <alduraarmanushaufaimron2@gmail.com>
2025-11-28 15:24:21 +0700

    Fix Status Bar Padding

--- app/src/main/java/com/alduraimron/accountinggrow/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/alduraimron/accountinggrow/presentation/home/HomeScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -323,6 +324,7 @@ fun HomeTopBar(
     Box(
         modifier = Modifier
             .fillMaxWidth()
+            .statusBarsPadding()
             .background(
                 brush = Brush.horizontalGradient(
                     colors = listOf(PrimaryBlue, PrimaryBlueLight)
